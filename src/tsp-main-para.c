d7d1b33333f1572c5255a3eb77872c0f7c6394c3
beljul <jullian.bellino@gmail.com>
2014-06-05 14:26:05 +0200

    merge

--- src/tsp-main-para.c
+++ src/tsp-main-para.c
@@ -140,19 +140,19 @@ int main (int argc, char **argv)
       args->sol_len = &sol_len;
 
       pthread_create(&threads[i], NULL, &tsp_thread, (void *)args);
-      printf("Thread %d created\n", i);
+      //printf("Thread %d created\n", i);
+      memcpy(sol, args->path, sizeof(tsp_path_t));
     }
 
     void *status;
     for(int i=0; i < nb_threads; ++i) {
-      printf("\nThread %d join begin\n", i);
+      //printf("\nThread %d join begin\n", i);
       pthread_join(threads[i], &status);
-      printf("Thread %d join end\n", i);
+      //printf("Thread %d join end\n", i);
     }
     
     clock_gettime (CLOCK_REALTIME, &t2);
 
-    memcpy(sol, solution, sizeof(tsp_path_t));
     if (affiche_sol)
       print_solution_svg (sol, sol_len);
 
